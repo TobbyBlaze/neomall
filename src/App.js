4c798a822e4b019498e2ec81f9dc68abe8d70902
Muritala Tobi <muritala.mt@gmail.com>
2020-06-05 04:14:21 +0000

    5-6-20

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ import Home from './components/Home';
 import Cart from './components/Cart';
 import Product from './components/Product';
 import Checkout from './components/Checkout';
-import Login from './components/Login';
-import Signup from './components/Signup';
 import Portal from './components/Portal';
 import Store from './components/Store';
 import Profile from './components/Profile';
@@ -28,23 +26,12 @@ export default class App extends Component{
         <div>
             {/* <Header /> */}
             <Switch>
-                {/* <Route exact path='/yummypizza/public/' component={Home} />
-                <Route exact path='/yummypizza/public/prdetails/:id' component={PrDetails} />
-                <Route exact path='/yummypizza/public/prdetails' component={PrDetails} />
-                <Route exact path='/yummypizza/public/shcart' component={ShCart} />
-                <Route exact path='/yummypizza/public/checkout' component={Checkout} />
-                <Route exact path='/yummypizza/public/login' component={Login} />
-                <Route exact path='/yummypizza/public/signup' component={Signup} />
-                <Route exact path='/yummypizza/public/thanks' component={Thanks} /> */}
-                {/* <Route component={Error} /> */}
-
+                
                 <Route exact path='/' component={Home} />
                 <Route exact path='/product/:id' component={Product} />
                 <Route exact path='/product' component={Product} />
                 <Route exact path='/cart' component={Cart} />
                 <Route exact path='/checkout' component={Checkout} />
-                <Route exact path='/login' component={Login} />
-                <Route exact path='/signup' component={Signup} />
                 <Route exact path='/thanks' component={Thanks} />
                 <Route exact path='/admine' component={Admine} />
                 <Route exact path='/portal' component={Portal} />

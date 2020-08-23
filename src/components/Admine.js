033c882094482942601f7d1e54729a933bf8c787
Muritala Tobi <muritala.mt@gmail.com>
2020-08-23 14:11:20 +0000

    date

--- src/components/Admine.js
+++ src/components/Admine.js
@@ -26,9 +26,9 @@ export default class Admine extends Component {
     //     this.setState({file: URL.createObjectURL(event.target.files[0])});
     // }
 
-    good_picsHandler = (event) => {
-        this.setState({good_pics1: URL.createObjectURL(event.target.files[0])});
-        this.setState({good_pics2: URL.createObjectURL(event.target.files[1])});
+    goodPicsHandler = (event) => {
+        this.setState({goodPics1: URL.createObjectURL(event.target.files[0])});
+        this.setState({goodPics2: URL.createObjectURL(event.target.files[1])});
     }
 
     submitHandler = e => {
@@ -161,48 +161,9 @@ export default class Admine extends Component {
                     </div>
                     </header>
 
-                <div className="limiter">
-                    <div className="container-login100">
-                        <div className="wrap-login100 p-t-30 p-b-50">
-                            <span className="login100-form-title p-b-41">
-                                Add goods here
-                            </span>
-                            {/* <form onSubmit={this.submitHandler} className="login100-form validate-form p-b-33 p-t-5">
-
-                            <input type="file" name="good_pics" onChange={this.fileHandler} class="btn btn-default text-center active form-control-file" title="Click to upload file"/>
-
-                                <div className="wrap-input100 validate-input" data-validate = "Enter email">
-                                    
-                                    <input className="input100" type="text" name="name" placeholder="name" value={name} onChange={this.changeHandler}  />
-                                    <span className="focus-input100" data-placeholder="&#xe82a;"></span>
-                                </div>
-
-                                <div className="wrap-input100 validate-input" data-validate="Enter password">
-                                    <input className="input100" type="text" name="description" placeholder="description" value={description} onChange={this.changeHandler}  />
-                                    <span className="focus-input100" data-placeholder="&#xe80f;"></span>
-                                </div>
-
-                                <div className="wrap-input100 validate-input" data-validate = "Enter email">
-                                    <input className="input100" type="number" name="price" placeholder="price" value={price} onChange={this.changeHandler}  />
-                                    <span className="focus-input100" data-placeholder="&#xe82a;"></span>
-                                </div>
-
-                                <div className="wrap-input100 validate-input" data-validate="Enter password">
-                                    <input className="input100" type="text" name="category" placeholder="category" value={category} onChange={this.changeHandler}  />
-                                    <span className="focus-input100" data-placeholder="&#xe80f;"></span>
-                                </div>
-
-                                <div className="container-login100-form-btn m-t-32">
-                                    <button type="submit" className="login100-form-btn">
-                                        Add good
-                                    </button>
-                                </div>
-                                
-                            </form> */}
-                        </div>
-                    </div>
-                </div>
-
+                    <br />
+                    <br />
+                    <br />
 
 
                 <section className="py-md-0">
@@ -265,12 +226,12 @@ export default class Admine extends Component {
 
                                                 <div className="form-group col-12">
                                                     <label htmlFor="good_pics">Good pics</label>
-                                                    <input type="file" name="good_pics[]" className="form-control" id="good_pics" onChange={this.good_picsHandler} multiple />
+                                                    <input type="file" name="goodPics[]" className="form-control" id="good_pics" onChange={this.goodPicsHandler} multiple />
                                                 </div>
-                                                <img class="img-responsive" src={this.state.good_pics1} />
+                                                <img class="img-responsive" src={this.state.goodPics1} />
                                                 <br />
                                                 <br />
-                                                <img class="img-responsive" src={this.state.good_pics2} />
+                                                <img class="img-responsive" src={this.state.goodPics2} />
 
                                                 <div className="col-12 mt-2">
                                                     <button type="submit" className="btn btn-block btn-primary">Add good</button>

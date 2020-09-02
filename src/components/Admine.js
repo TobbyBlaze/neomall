d9cd459823caa8514f61ac3cca3adc987589ece9
Muritala Tobi <muritala.mt@gmail.com>
2020-09-02 16:19:40 +0000

    date

--- src/components/Admine.js
+++ src/components/Admine.js
@@ -237,11 +237,7 @@ export default class Admine extends Component {
                                                     <label htmlFor="goodPics">Good pics</label>
                                                     <input type="file" name="file[]" className="form-control" id="goodPics" onChange={this.goodPicsHandler} multiple />
                                                 </div>
-                                                <img class="img-responsive" src={this.state.file1} />
-                                                <br />
-                                                <br />
-                                                <img class="img-responsive" src={this.state.file2} />
-
+                                                
                                                 <div className="col-12 mt-2">
                                                     <button type="submit" className="btn btn-block btn-primary">Add good</button>
                                                 </div>
@@ -250,6 +246,12 @@ export default class Admine extends Component {
 
                                         </div>
                                     </form>
+
+                                    <img class="img-responsive" src={this.state.file1} />
+                                                <br />
+                                                <br />
+                                                <img class="img-responsive" src={this.state.file2} />
+
                                 </div>
                             </div>
                         </div>

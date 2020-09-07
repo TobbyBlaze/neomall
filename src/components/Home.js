992b408dd8199f823e4d34dc3869e5217cb9af32
Muritala Tobi <muritala.mt@gmail.com>
2020-09-07 15:28:43 +0000

    date

--- src/components/Home.js
+++ src/components/Home.js
@@ -521,8 +521,8 @@ export default class Home extends Component{
                         <div className="product">
                         <figure className="product-image">
                             <Link to={"product/"+good.id}>
-                            <img src={"https://neomallapi.herokuapp.com/file/"+good.image[0]} alt="Image" />
-                            <img src={"https://neomallapi.herokuapp.com/file/"+good.image[1]} alt="Image" />
+                            <img src={"https://neomallapi.herokuapp.com/file/"+json.decode(good.image[0])} alt="Image" />
+                            <img src={"https://neomallapi.herokuapp.com/file/"+json.decode(good.image[1])} alt="Image" />
                             </Link>
                         </figure>
                         <div className="product-meta">

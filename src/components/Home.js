6b2ac1bfaa07c3b4640d6326c7d07bbd75bec86f
Muritala Tobi <muritala.mt@gmail.com>
2020-09-07 04:29:09 +0000

    date

--- src/components/Home.js
+++ src/components/Home.js
@@ -253,29 +253,30 @@ export default class Home extends Component{
         }
 
         return(
-            <InfiniteScroll
-                dataLength={items.length} //This is important field to render the next data
-                next={fetchData}
-                hasMore={true}
-                loader={<h4>Loading...</h4>}
-                endMessage={
-                    <p style={{textAlign: 'center'}}>
-                    <b>Yay! You have seen it all</b>
-                    </p>
-                }
-                // below props only if you need pull down functionality
-                refreshFunction={this.refresh}
-                pullDownToRefresh
-                pullDownToRefreshContent={
-                    <h3 style={{textAlign: 'center'}}>&#8595; Pull down to refresh</h3>
-                }
-                releaseToRefreshContent={
-                    <h3 style={{textAlign: 'center'}}>&#8593; Release to refresh</h3>
-                }>
-                {items}
-            </InfiniteScroll>
+            
             
             <div>
+                <InfiniteScroll
+                    dataLength={items.length} //This is important field to render the next data
+                    next={fetchData}
+                    hasMore={true}
+                    loader={<h4>Loading...</h4>}
+                    endMessage={
+                        <p style={{textAlign: 'center'}}>
+                        <b>Yay! You have seen it all</b>
+                        </p>
+                    }
+                    // below props only if you need pull down functionality
+                    refreshFunction={this.refresh}
+                    pullDownToRefresh
+                    pullDownToRefreshContent={
+                        <h3 style={{textAlign: 'center'}}>&#8595; Pull down to refresh</h3>
+                    }
+                    releaseToRefreshContent={
+                        <h3 style={{textAlign: 'center'}}>&#8593; Release to refresh</h3>
+                    }>
+                    {items}
+                </InfiniteScroll>
                 
                 <div className="container-fluid">
                     {/* <!-- header --> */}

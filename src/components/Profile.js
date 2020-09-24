cb491d5d6a0b818c7627e33190a1f2082867854d
Muritala Tobi <muritala.mt@gmail.com>
2020-09-24 17:13:51 +0000

    profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -242,7 +242,7 @@ export default class Profile extends Component{
     }
 
     render(){
-        const { user, location, goods, carts, cartsNum, delcart, errorMsg, loading } = this.state;
+        const { user, location, goods, carts, cartsNum, delcart, errorMsg, loading, old_password, password, confirm_password } = this.state;
         // const { name, last_name, email, old_password, password, confirm_password, city, country, street, zip, phone } = this.state.user;
 
         var a=localStorage.getItem("authen");
